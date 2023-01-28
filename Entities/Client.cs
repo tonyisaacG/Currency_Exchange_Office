0c8d099d06a069d9a002bf790956bb2ebf34e002
tonyisaacG <antonyisacegayed@gmail.com>
2023-01-28 12:30:58 +0200

    add entities for database (code first)

---
+++ Entities/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Client
+    {
+        public int Client_id { get; set; }
+        public string Client_name { get; set; }
+        public string Client_phoneNum { get; set; }
+        public decimal Client_money { get; set; }
+    }
+}
