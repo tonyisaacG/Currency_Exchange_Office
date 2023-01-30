ef22feef6bc4d365218a689a23a52a6d0aaf1ee4
tonyisaacG <106390346+tonyisaacG@users.noreply.github.com>
2023-01-30 03:52:57 +0200

    Update Program.cs

--- Currency_Exchange_Office/Program.cs
+++ Currency_Exchange_Office/Program.cs
@@ -11,7 +11,7 @@ namespace Currency_Exchange_Office
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Purchases_Frm());
+          ///  Application.Run(new Purchases_Frm());
         }
     }
 }
